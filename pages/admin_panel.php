993c5f1dc496156fc82520e5e26b9b55f7cf862c
Tusski <aalmalel@yahoo.com>
2023-06-09 22:43:16 +0800

    main

--- pages/admin_panel.php
+++ pages/admin_panel.php
@@ -23,18 +23,13 @@ $result = Product::list();
     <title>Product Management</title>
     <style>
         body {
-           font-family: Arial, sans-serif;
+            font-family: Arial, sans-serif;
         }
 
         .container {
             max-width: 800px;
             margin: 0 auto;
             padding: 20px;
-            position: relative;
-            display: flex;
-            flex-direction: row;
-            right: 100px;
-            top: 80px;
         }
 
         h1 {
@@ -43,9 +38,6 @@ $result = Product::list();
         }
 
         .logout-button {
-            position: absolute;
-            top: 10px;
-            right: 10px;
             padding: 6px 12px;
             font-size: 14px;
             font-weight: bold;
@@ -54,13 +46,6 @@ $result = Product::list();
             border: none;
             border-radius: 4px;
             cursor: pointer;
-        }
-
-        .product-container {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            justify-content: flex-start;
         }
 
         .product-container {
@@ -126,56 +111,63 @@ $result = Product::list();
             background-color: #4CAF50;
             color: #fff;
         }
+
+        .button-container {
+            display: flex;
+            justify-content: space-between;
+        }
     </style>
 </head>
 <body>
-    <h1>Product Management</h1>
-        <a href="logout.php" class="logout-button">Logout</a>
-        <div class="container">
-        <?php foreach($result as $res): ?>
+    <div class="container">
+        <h1>Product Management</h1>
+        <div class="button-container">
+            <a href="add_product.php" class="button add">Add Product</a>
+            <a href="logout.php" class="logout-button">Logout</a>
+        </div>
+
         <div class="product-container">
-        <div class="product-card">
-            <img src='../images/<?php echo $res->getImage();?>' alt="Product 1" class="product-image">
-            <div class="product-details">
-                <div class="product-name">
-                    <label>Name: </label>
-                    <?=$res->getProdName(); ?>
-                </div>
-                <div class="product-price">
-                    <label>Price: Php</label>
-                    <?=$res->getPrice();?>
-                </div>
-                <div>
-                    <label>Description: </label>
-                    <?php echo $res->getDescription();?>
-                </div>
-                <div>
-                    <label>Quantity: </label>
-                    <?php echo $res->getQuantity();?>
-                </div>
-                <div>
-                    <label>Size: </label>
-                    <?php echo $res->getSize();?>
-                </div>
-                <div>
-                    <label>Color: </label>
-                    <?php echo $res->getColor();?>
-                </div>
-                <div>
-                    <label>Gender: </label>
-                    <?php echo $res->getGender();?>
-                </div>
-                <div class="product-actions">
-                    <a href="edit_product.php?id=<?php echo $res->getProdID();?>" class="button edit">Edit</a>
-                    <a href="delete_product.php?id=<?php echo $res->getProdID();?>" class="button delete">Delete</a>
+            <?php foreach($result as $res): ?>
+            <div class="product-card">
+                <img src='../images/<?php echo $res->getImage();?>' alt="Product 1" class="product-image">
+                <div class="product-details">
+                    <div class="product-name">
+                        <label>Name: </label>
+                        <?=$res->getProdName(); ?>
+                    </div>
+                    <div class="product-price">
+                        <label>Price: Php</label>
+                        <?=$res->getPrice();?>
+                    </div>
+                    <div>
+                        <label>Description: </label>
+                        <?php echo $res->getDescription();?>
+                    </div>
+                    <div>
+                        <label>Quantity: </label>
+                        <?php echo $res->getQuantity();?>
+                    </div>
+                    <div>
+                        <label>Size: </label>
+                        <?php echo $res->getSize();?>
+                    </div>
+                    <div>
+                        <label>Color: </label>
+                        <?php echo $res->getColor();?>
+                    </div>
+                    <div>
+                        <label>Gender: </label>
+                        <?php echo $res->getGender();?>
+                    </div>
+                    <div class="product-actions">
+                        <a href="edit_product.php?id=<?php echo $res->getProdID();?>" class="button edit">Edit</a>
+                        <a href="delete_product.php?id=<?php echo $res->getProdID();?>" class="button delete">Delete</a>
+                    </div>
                 </div>
             </div>
+            <?php endforeach?>
         </div>
-        </div>
-        <?php endforeach?>
-        <!-- Add more product cards as needed -->
     </div>
-    <a href="add_product.php" class="button add">Add Product</a>
-
 </body>
 </html>
+
